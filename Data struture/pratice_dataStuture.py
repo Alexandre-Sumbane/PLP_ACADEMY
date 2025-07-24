66d12c59bf362e7adadc5632353e528554e9dbef
Alexandre Sumbane <alexandresumbane7@gmail.com>
2025-07-24 13:12:18 +0200

    Estrutura Dicionario

---
+++ Data struture/pratice_dataStuture.py
@@ -0,0 +1,64 @@
+# Dicionarios
+
+lista_productos = ['airpod', 'ipad', 'iphone', 'macbook'];
+precos =[2000, 9000, 6000, 11000];
+
+disc_produtos = {
+    "airpod": 2000,
+    "ipad": 9000,
+    "iphone": 6000,
+    "macbook": 11000
+}
+
+# # editar um elemento
+# disc_produtos["iphone"] = disc_produtos["iphone"] * 1.3
+
+# # Pegar um elemento
+# print(disc_produtos)
+
+# # quantidade de itens
+# print(len(disc_produtos))
+
+# # retirar um item do dicionario
+
+# disc_produtos.pop("airpod")
+
+# print(disc_produtos)
+
+# Adicionar um item no dicionario
+
+# disc_produtos['apple watch'] = 2500
+
+# print(disc_produtos)
+
+# verificar se um item existe no dicionario
+# if "iphone" in disc_produtos
+#  print("existe produto")
+# else
+#     print("nao existe")
+
+ # Verificar se um valor existe nos valores do dicionario
+#  if 9000 in disc_produtos:
+#  print("existe produto")
+# else:
+#     print("nao existe")    
+
+nome_produto = input("Nome do produto:");
+preco_produto = input("Preço do produto:");
+
+# Cadastrar o novo Produto (se o produto nao existir)
+nome_produto = nome_produto.lower()
+preco_produto = float(preco_produto)
+
+disc_produtos[nome_produto] = preco_produto
+
+
+# Actualizar o preço de todos productos
+produto = 'ipad'
+
+for produto in disc_produtos:
+    novo_preco = disc_produtos[produto] *1.1
+    disc_produtos[produto] = novo_preco
+    
+
+print(disc_produtos)
